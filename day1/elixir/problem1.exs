1af7129aedf47cba60e9787c81853fb4796dde97
pgermishuys <pieter@pieterg.com>
2018-12-04 21:52:17 +0100

    Add solution for part 2 of day 1

--- day1/elixir/problem1.exs
+++ day1/elixir/problem1.exs
@@ -25,3 +25,22 @@ contents
 |> Stream.map(fn x -> String.to_integer(x) end)
 |> Enum.sum
 |> IO.inspect
+
+# Problem 2 with file (more efficient alternative by providing "recipies")
+{:ok, contents} = File.read("../input.txt")
+contents
+|> String.splitter("\n", trim: true)
+|> Stream.map(fn x -> 
+    {number, _dontcare} = Integer.parse(x)
+    number
+   end)
+|> Stream.cycle()
+|> Enum.reduce_while({0, [0]}, fn x, {current_frequency, frequencies} ->
+    new_frequency = current_frequency + x
+    if new_frequency in frequencies do
+        {:halt, new_frequency}
+    else
+        {:cont, {new_frequency, [new_frequency | frequencies]}}
+    end
+   end)
+|> IO.puts 
